5da4dc6db2c39f2e759e89436aed17553d5854f4
Louis Bustin <Louis.Bustin@powerschool.com>
2021-01-14 19:46:05 -0600

    All mongo and API stuff working. Added logging

--- src/api/controllers/user.controller.ts
+++ src/api/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import User, { IUser } from '../models/user.model';
+import logger from '../../logger';
 
 import { Request, Response } from 'express';
 
@@ -7,30 +8,45 @@ class UserController {
         User.find({}).then((users) => {
             return response.status(200).json(users);
          }).catch((e: any) => {
-             console.log(e);
+             logger.error(e);
              return response.status(500).send();
          });
     }
     
     static getOne = (request: Request, response: Response) => {
-        return response.status(200).json({"works": true});
+        User.findById(request.params.userId)
+        .then((user:IUser) => {
+            return response.status(200).json(user);
+        }).catch((e: any) => {
+            logger.error(e);
+            return response.status(500).send();
+        })
     }
 
     static create = (request: Request, response: Response) => {
         User.create(request.body).then((user: IUser) => {
             return response.status(200).json(user);
         }).catch((e: any) => {
-            console.log(e);
+            logger.error(e);
             return response.status(500).send();
         });
     }
 
     static update = (request: Request, response: Response) => {
-        return response.status(200).json({"works": true});
+        User.findByIdAndUpdate(request.params.userId, request.body).then((user) => {
+            return response.status(200).send();
+        }).catch((e: any) => {
+            logger.error(e);
+        });
     }
 
     static delete = (request: Request, response: Response) => {
-        return response.status(200).json({"works": true});
+        User.findByIdAndDelete(request.params.userId).then(() => { 
+            return response.status(200).send();
+        }).catch((e: any) => {
+            logger.error(e);
+            return response.status(500).send();
+        });
     }
     
 }
