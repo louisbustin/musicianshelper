2626636b1200f1c5ecff34d2602fdcbe1569d2fc
Louis Bustin <Louis.Bustin@powerschool.com>
2021-01-16 17:49:10 -0600

    Fixing production builds

--- config/database.ts
+++
@@ -1,5 +0,0 @@
-
-const dbconfig = {
-    'database': `mongodb+srv://bracketwebmongo:${process.env.mongodbpass}@cluster0.v3yjo.mongodb.net/bracketweb?retryWrites=true&w=majority`
-  };
-export { dbconfig };
