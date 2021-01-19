6b7330016fd815fec6d7f91c75507955098bcb55
Louis Bustin <Louis.Bustin@powerschool.com>
2021-01-19 07:38:41 -0600

    Add UserForm in a dialog User info displays in dialog Edit not quite working.

---
+++ src/app/admin/users/user-form/user-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UserService } from 'src/app/services/user.service';
+
+import { User } from '../../../models/user';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss']
+})
+export class UserFormComponent implements OnInit {
+
+  model: User = null;
+
+  hidden = false;
+  
+  constructor(@Inject(MAT_DIALOG_DATA) public data: User, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.model = this.data;
+  }
+
+  onSubmit() {
+    this.userService.update(this.model);
+  }
+}
--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { User } from '../../models/user';
 import { UserService } from 'src/app/services/user.service';
+import { UserFormComponent } from './user-form/user-form.component';
+import { ButtonCellRendererComponent } from '../../button-cell-renderer.component';
 
 @Component({
   selector: 'app-users',
@@ -11,15 +15,44 @@ export class UsersComponent implements OnInit {
   userData: any;
   
   columnDefs = [
+    {
+      field: "",
+      cellRenderer: "btnCellRenderer",
+      cellRendererParams: {
+        clicked: (rowData: any) => {
+          console.log(rowData);
+          this.openDialog(rowData);
+        },
+        buttonText: "Edit",
+        buttonClass: "button is-link",
+        containerStyle: "display: flex; justify-content: center; align-items: center; height: 100%;"
+      },
+      width: 100
+    },
     { field: 'email', sortable: true, filter: true },
     { field: 'firstName', sortable: true, filter: true },
     { field: 'lastName', sortable: true, filter: true }
-  ];
+  ]; 
+  frameworkComponents = {
+    btnCellRenderer: ButtonCellRendererComponent
+  }
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private dialog: MatDialog) {   
+  }
 
   ngOnInit(): void {
     this.userData = this.userService.getAll();
   }
+
+
+  openDialog(user: any) {
+    const dialogRef = this.dialog.open(UserFormComponent, { data: user });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+
 
 }
