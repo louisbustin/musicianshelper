efd08eb816014070450c9e6308d1cbc5b951f4a3
Louis Bustin <louis@eforge.us>
2021-02-06 18:44:02 -0600

    Began using dotenv for system specific configurations

--- src/environments/database.ts
+++ src/environments/database.ts
@@ -1,4 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const dbconfig = {
-     'database': `mongodb+srv://bracketwebmongo:${process.env.mongodbpass}@cluster0.v3yjo.mongodb.net/bracketweb?retryWrites=true&w=majority`,
+   'database': `mongodb+srv://bracketwebmongo:${process.env.bracketwebmongopass}@cluster0.v3yjo.mongodb.net/bracketweb?retryWrites=true&w=majority`,
   };
 
