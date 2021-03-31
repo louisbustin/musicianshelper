f74ddf3a7e2c58ca73615ec5046178b172ea10a9
Louis Bustin <Louis.Bustin@powerschool.com>
2021-03-30 20:23:56 -0500

    Remove references to brackets

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,11 +2,9 @@ import express from 'express';
 
 //import postRoutes from './post.route';
 import userRoutes from './users.route';
-import bracketGroupRoutes from './bracket-group.route'
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
-router.use('/bracketgroups', bracketGroupRoutes);
 
 export { router };
