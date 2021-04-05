34117bbec8d1f213d44acb8f6aec301e0f3bd5bb
louisbustin <louis.bustin@powerschool.com>
2021-04-04 20:30:50 -0500

    Redesign. Reorganize into seprate modules for Shared and Navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { AgGridModule } from 'ag-grid-angular';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { appRoutes, appDeclarations } from './app.routes';
 import { UserFormComponent } from './admin/users/user-form/user-form.component';
 import { UsersComponent } from './admin/users/users.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonCellRendererComponent } from './button-cell-renderer.component';
 import { AuthModule } from '@auth0/auth0-angular';
 import { environment } from '../environments/environment';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthHttpInterceptor } from '@auth0/auth0-angular';
+import { SharedModule } from './shared/shared.module';
+import { NavigationModule } from './navigation/navigation.module';
+import { ButtonCellRendererComponent } from './shared/button-cell-renderer/button-cell-renderer.component';
 
 
 @NgModule({
   declarations: appDeclarations,
   imports: [
-    CommonModule,
-    HttpClientModule,
-    FormsModule,
+    SharedModule,
+    NavigationModule,
     MatDialogModule,
     BrowserAnimationsModule,
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { AuthenticationButtonComponent } from './authentication-button/authentication-button.component';
+import { LoginButtonComponent } from './login-button/login-button.component';
+import { LogoutButtonComponent } from './logout-button/logout-button.component';
+import { SignupButtonComponent } from './signup-button/signup-button.component';
+import { AuthenticatedNavlinksComponent } from './authenticated-navlinks/authenticated-navlinks.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+
+@NgModule({
+  declarations: [
+    AuthenticatedNavlinksComponent,    
+    AuthenticationButtonComponent,
+    LoginButtonComponent,
+    LogoutButtonComponent,
+    SignupButtonComponent,
+    TopNavComponent,
+    LeftNavComponent
+  ],
+  imports: [
+    SharedModule
+  ],
+  exports: [
+    TopNavComponent,
+    LeftNavComponent
+  ]
+})
+export class NavigationModule { }
